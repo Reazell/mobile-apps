ac73595758f35d2942e3be13a16681b9d51d1e20
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2018-03-16 10:21:40 +0100

    16.03.2018 WinForms + WPF lists

---
+++ WPF/App/App/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using ClassLibrary1;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ WPF/App/ClassLibrary1/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Windows.Input;
+
+namespace ClassLibrary1
+{
+    public class MainViewModel
+    {
+        private int x = 1;
+        public ObservableCollection<int> myList { get; set; } = new ObservableCollection<int>();
+
+        public void AddItem()
+        {
+            myList.Add(x);
+            x++;
+        }
+
+        public ICommand addItemCommand => new Command(AddItem);
+    }
+
+    public class Command : ICommand
+    {
+        private readonly Action _byleco;
+
+        public Command(Action byleco)
+        {
+            _byleco = byleco;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _byleco.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
