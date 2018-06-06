7701836824542228b5969e13bd7620151da4ed06
Reazell <szymon.przybyszewski94@gmail.com>
2018-06-06 15:14:35 +0200

    Add files via upload

---
+++ Classes/App1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            LabelTest.BackgroundColor = Color.DarkBlue;
+		}
+
+        public async void btnOk_Clicked(object sender, EventArgs e)
+        {
+            var text = entryParam.Text;
+
+            if (await DisplayAlert("Czy na pewno?", "Czy na pewno chciales wpisac " + text + " ?" , "Tak", "Nie"))
+            {
+                await Navigation.PushModalAsync(new SecondPage());
+            }
+            var button = (Button)sender;
+            await DisplayAlert("OK", "Popraw dane", "OK");
+        }
+	}
+}
